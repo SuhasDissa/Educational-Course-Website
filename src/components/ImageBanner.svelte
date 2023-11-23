e1b23f82a934a3a46cadcb3e796c05f068843626
Suhas Dissanayake <suhasdissa@gmail.com>
2023-11-23 19:39:28 +0530

    Second Commit

---
+++ src/components/ImageBanner.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+	export let imageUrl: string;
+</script>
+
+<div
+	class="relative w-full h-96 rounded-xl overflow-hidden bg-contain bg-no-repeat bg-right duration-300"
+	style="background-image:url('{imageUrl}');"
+>
+	<div class="h-full w-full p-10 bg-gradient-to-r dark:from-slate-800 from-slate-300 from-60%">
+		<h1 class="text-slate-700 dark:text-slate-300 text-2xl sm:text-3xl md:text-4xl font-bold w-[60%]">
+			{title}
+		</h1>
+		<p class=" text-slate-400 text-lg sm:text-xl md:text-2xl font-bold max-w-prose pt-4 sm:pt-8  w-[60%]">
+			{description}
+		</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,57 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import ImageBanner from '../components/ImageBanner.svelte';
+	let slides = [
+		{
+			title: 'අපගේ දැක්ම',
+			description: 'පරිපුර්ණ අධ්‍යාපනයක් තුළින් විශිෂ්ට දරු පරපුරක්'
+		},
+		{
+			title: 'අපගේ මෙහෙවර',
+			description:
+				'යමබර හා පුළුල් අධ්‍යාපන ක්‍රියාවලියක් සම්පාදනය කිරීම තුළින් ඌව පළාතේ විශිෂ්ට දරු පරපුරක් බිහීකිරීම සදහා අධ්‍යාපන ක්ෂේත්‍රයේ සියලු භෞතික,මානව හා මුල්‍ය සම්පත් කාර්යක්ෂමව හා ඵලදායී ලෙස මෙහෙයවීමයි.'
+		},
+		{
+			title: 'සාදරයෙන් පිළිගනිමු',
+			description:
+				'බහුතල ඉගෙනුම් ක්‍රමවේදය පිළිබඳ මාර්ගගත ගුරු පුහුණු පාඨමාලාවට ඔබව සාදරයෙන් පිළිගනිමු'
+		}
+	];
+	let images = [
+		'images/children/01.jpg',
+		'images/children/02.jpg',
+		'images/children/03.jpg',
+		'images/children/04.jpg',
+		'images/children/05.jpg',
+		'images/children/06.jpg',
+		'images/children/07.jpg'
+	];
+	let currentSlide = 0;
+	let currentImage = 0;
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			currentSlide = (currentSlide + 1) % slides.length;
+			currentImage = (currentImage + 1) % images.length;
+		}, 5000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
 <div class="mx-auto max-w-screen-xl px-8 py-8 flex flex-col justify-center content-center h-full">
 	<section class="pt-10 flex flex-row justify-evenly">
-	<img class="h-32" src="images/srilanka.png" alt="" srcset="">
-	<img class="h-32" src="images/Banner.jpg" alt="" srcset="">
-	<img class="h-32" src="images/unicef.png" alt="" srcset="">
+		<img class="h-32" src="images/uvaflag.png" alt="" srcset="" />
+		<img class="h-32" src="images/banner.png" alt="" srcset="" />
+		<img class="h-32" src="images/unicef.png" alt="" srcset="" />
+	</section>
+	<section class="pt-10 flex flex-row justify-evenly">
+		<ImageBanner
+			title={slides[currentSlide].title}
+			description={slides[currentSlide].description}
+			imageUrl={images[currentImage]}
+		/>
 	</section>
 </div>
