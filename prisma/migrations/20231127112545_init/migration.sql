39bd50a1488e907a983ec89d972aeba677670451
Suhas Dissanayake <suhasdissa@gmail.com>
2023-11-27 16:58:29 +0530

    database

--- prisma/migrations/20231127112545_init/migration.sql
+++ prisma/migrations/20231127112545_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "progress" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "userId" TEXT NOT NULL,
     "module1" BOOLEAN NOT NULL DEFAULT false,
     "module2" BOOLEAN NOT NULL DEFAULT false,
@@ -11,35 +11,40 @@ CREATE TABLE "progress" (
     "module7" BOOLEAN NOT NULL DEFAULT false,
     "module8" BOOLEAN NOT NULL DEFAULT false,
     "practical" BOOLEAN NOT NULL DEFAULT false,
-    CONSTRAINT "progress_userId_fkey" FOREIGN KEY ("userId") REFERENCES "auth_user" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "progress_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "auth_user" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "id_no" TEXT NOT NULL,
     "school" TEXT NOT NULL,
-    "role" TEXT NOT NULL DEFAULT 'user'
+    "role" TEXT NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "auth_user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "auth_session" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "user_id" TEXT NOT NULL,
     "active_expires" BIGINT NOT NULL,
     "idle_expires" BIGINT NOT NULL,
-    CONSTRAINT "auth_session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "auth_session_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "auth_key" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "hashed_password" TEXT,
     "user_id" TEXT NOT NULL,
-    CONSTRAINT "auth_key_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "auth_key_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -62,3 +67,12 @@ CREATE UNIQUE INDEX "auth_key_id_key" ON "auth_key"("id");
 
 -- CreateIndex
 CREATE INDEX "auth_key_user_id_idx" ON "auth_key"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "progress" ADD CONSTRAINT "progress_userId_fkey" FOREIGN KEY ("userId") REFERENCES "auth_user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth_session" ADD CONSTRAINT "auth_session_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "auth_key" ADD CONSTRAINT "auth_key_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth_user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
