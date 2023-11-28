74b375e5e3ce561c2d42793d92fb6cf1a96c951a
Suhas Dissanayake <suhasdissa@gmail.com>
2023-11-28 23:00:16 +0530

    add modules

--- src/lib/server/modules.ts
+++ src/lib/server/modules.ts
@@ -3,56 +3,56 @@ import type { Module } from "./types";
 export let modules: Module[] = [
     {
         id: 1,
-        url: "http://minlshtest123.rf.gd/Sinhala/web/Module/multi_level_learning/story.html",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module1/story.html",
         name: "Module 1",
         description: "බහුතල ඉගෙනුම් ක්‍රමවේදය හදුනා ගැනීම",
         progress: 0
     },
     {
         id: 2,
-        url: "",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module2/story.html",
         name: "Module 2",
         description: "බහුතල ඉගෙනුම් ක්‍රමවේදයේ මූලදර්ම හදුනා ගැනීම",
         progress: 0
     },
     {
         id: 3,
-        url: "",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module3/story.html",
         name: "Module 3",
         description: "පොදු තක්සේරුකරණය",
         progress: 0
     },
     {
         id: 4,
-        url: "",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module4/story.html",
         name: "Module 4",
         description: "ගතික කණ්ඩායම්",
         progress: 0
     },
     {
         id: 5,
-        url: "",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module5/story.html",
         name: "Module 5",
         description: "ඉගෙනුම් ඉනිමග",
         progress: 0
     },
     {
         id: 6,
-        url: "",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module6/story.html",
         name: "Module 6",
         description: "ප්‍රතිකාර්‍ය හා ඉදිරි පෝෂණය",
         progress: 0
     },
     {
         id: 7,
-        url: "",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module7/story.html",
         name: "Module 7",
         description: "ආදර්ශ පාඩම් සලසුම් හා ආදර්ශ පාඩම",
         progress: 0
     },
     {
         id: 8,
-        url: "",
+        url: "https://pasz.edu.gov.lk/SInhala/multi_level_modules/module8/story.html",
         name: "Module 8",
         description: "බහුතල පන්ති කාමරය සැලසුම් කිරීම",
         progress: 0
