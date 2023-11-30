b3cf54323dc748e3ea7069429a641de42ad34f7b
Suhas Dissanayake <suhasdissa@gmail.com>
2023-11-30 20:17:01 +0530

    add persitent language switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import '../app.css';
 	import Navigation from '../components/Navigation.svelte';
+	import { onMount } from 'svelte';
+	import { locale } from '$lib/i18n';
+
+	onMount(async () => {
+		const storedLocale = localStorage.getItem('locale');
+		if (storedLocale) {
+			locale.set(storedLocale);
+		}
+		locale.subscribe((value) => {
+			localStorage.setItem('locale', value);
+		});
+	});
 </script>
 
 <div class="font-sans antialiased bg-slate-100 dark:bg-slate-900 min-h-screen pt-16">
