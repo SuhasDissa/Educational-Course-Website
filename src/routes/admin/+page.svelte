f01bf0accb130d34ac64ee8ee5b2a33754da118e
Suhas Dissanayake <suhasdissa@gmail.com>
2023-12-01 17:00:10 +0530

    add search for admin

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -4,6 +4,17 @@
 
 	import { t } from '$lib/i18n';
 	import StarProgress from '../../components/StarProgress.svelte';
+
+	let searchTerm = '';
+	let filteredUsers = [];
+
+	function searchAdditives() {
+		filteredUsers = users.filter((user) => {
+			let name = user.name.toLowerCase();
+			let username = user.username.toLocaleLowerCase();
+			return name.includes(searchTerm.toLowerCase()) || username.includes(searchTerm.toLowerCase());
+		});
+	}
 </script>
 
 <div class="mx-auto px-8 py-8 flex flex-col justify-center content-center h-full">
@@ -11,16 +22,28 @@
 		{$t('admin_page')}
 	</h1>
 	<section class="pt-10 flex flex-col">
-		<h5 class="p-6 text-2xl font-bold tracking-tight text-slate-700 dark:text-slate-300">
-			Registered Users
-		</h5>
-		<div class="relative overflow-x-auto shadow-md sm:rounded-lg dark:bg-slate-800">
+		<div class="flex flex-row gap-8">
+			<h5 class="p-6 text-2xl font-bold tracking-tight text-slate-700 dark:text-slate-300">
+				Registered Users
+			</h5>
+			<div class="sm:flex flex-row items-center hidden">
+				<input
+					bind:value={searchTerm}
+					on:input={searchAdditives}
+					class="leading-5 py-2 px-3 bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+					placeholder="Search Name or Id No."
+				/>
+			</div>
+		</div>
+
+		<div class="relative overflow-x-auto shadow-md rounded-lg dark:bg-slate-800">
 			<table class="w-full text-md text-left rtl:text-right text-slate-500 dark:text-slate-400">
 				<thead class="text-slate-700 bg-slate-50 dark:bg-slate-700 dark:text-slate-400">
 					<tr>
 						<th scope="col" class="px-6 py-3"> Name </th>
 						<th scope="col" class="px-6 py-3"> School </th>
 						<th scope="col" class="px-6 py-3"> Phone No. </th>
+						<th scope="col" class="px-6 py-3"> Id No. </th>
 						<th scope="col" class="px-6 py-3"> Modules </th>
 						<th scope="col" class="px-6 py-3"> Practical </th>
 						<th scope="col" class="px-6 py-3"> Role </th>
@@ -30,13 +53,14 @@
 					</tr>
 				</thead>
 				<tbody>
-					{#each users as user}
+					{#each searchTerm.length == 0 ? users : filteredUsers as user}
 						<tr
 							class="border-b dark:border-slate-700 font-medium text-slate-900 whitespace-nowrap dark:text-slate-100"
 						>
 							<td class="px-6 py-4"> {user.name} </td>
 							<td class="px-6 py-4"> {user.school} </td>
 							<td class="px-6 py-4"> {user.phone} </td>
+							<td class="px-6 py-4"> {user.username} </td>
 							<td class="px-6 py-4"> <StarProgress progress={user.progress} /> </td>
 							<td class="px-6 py-4">
 								{#if user.progress?.practical}
