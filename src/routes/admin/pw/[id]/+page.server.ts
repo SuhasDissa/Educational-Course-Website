9fade23644232361d837a9747eb1bafc55dc94fa
Suhas Dissanayake <suhasdissa@gmail.com>
2023-12-11 13:01:41 +0530

    add password change option

---
+++ src/routes/admin/pw/[id]/+page.server.ts
@@ -0,0 +1,42 @@
+import { error, fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { auth } from '$lib/server/lucia';
+import { prisma } from '$lib/server/prisma';
+
+export const load: PageServerLoad = async ({ params, locals }) => {
+    const session = await locals.auth.validate()
+    if (!session) {
+        throw redirect(302, '/login')
+    }
+    const user = await prisma.authUser.findUnique({
+        where: {
+            id: params.id
+        }
+    }
+    )
+    if (!user) {
+        throw error(404, 'User not found')
+    }
+    return {
+        user: user
+    }
+}
+
+export const actions = {
+    default: async ({ request }) => {
+        const formData = Object.fromEntries(await request.formData());
+        const data = formData as {
+            username: string;
+            password: string;
+        };
+
+        try {
+            const user = await prisma.authUser.findUnique
+            auth.updateKeyPassword("username", data.username.toUpperCase(), data.password)
+        } catch (err) {
+            console.error(err)
+            return fail(400, { message: 'Could not update user' })
+        }
+        throw redirect(302, '/admin')
+    }
+}
---
+++ src/routes/admin/pw/[id]/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let data;
+	let user = data.user;
+	import { t } from '$lib/i18n';
+</script>
+
+<div class="flex justify-center content-center w-full pb-16 px-8 pt-16 sm:pt-8">
+	<form class="w-full max-w-xl" method="post" enctype="multipart/form-data">
+		<input value={user.username} name="username" type="hidden" required />
+		<div class="mb-6">
+			<h5 class="text-3xl font-bold tracking-tight text-slate-700 dark:text-slate-300">
+				Change Password
+			</h5>
+		</div>
+		<div class="mb-6">
+			<label for="password" class="block mb-2 text-sm font-bold text-slate-900 dark:text-slate-200"
+				>{$t('user.password')}</label
+			>
+			<input
+				name="password"
+				type="password"
+				id="password"
+				class="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg focus:ring-slate-500 focus:border-slate-500 block w-full p-2.5 dark:bg-slate-700 dark:border-slate-600 dark:placeholder-slate-400 dark:text-white dark:focus:ring-slate-500 dark:focus:border-slate-500"
+				placeholder=""
+				required
+			/>
+		</div>
+		<button
+			type="submit"
+			class="text-white bg-slate-700 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-bold rounded-lg text-md w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
+			>Save</button
+		>
+	</form>
+</div>
