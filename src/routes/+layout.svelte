3af510ecd1aea826af21be21d5fce730a50afa02
Suhas Dissanayake <suhasdissa@gmail.com>
2024-03-20 15:23:41 +0530

    Add Progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Navigation from '../components/Navigation.svelte';
 	import { onMount } from 'svelte';
 	import { locale } from '$lib/i18n';
+	import { ProgressBar } from '@prgm/sveltekit-progress-bar';
 
 	onMount(async () => {
 		const storedLocale = localStorage.getItem('locale');
@@ -14,6 +15,8 @@
 		});
 	});
 </script>
+
+<ProgressBar class="text-slate-500" zIndex={100}/>
 
 <div class="min-h-screen bg-slate-100 pt-16 font-sans antialiased dark:bg-slate-900">
 	<Navigation />
